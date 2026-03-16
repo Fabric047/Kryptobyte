b746ac7ede6220e62bc6b83c1e218483fcf94baf
Fabricio <157657298+Fabric047@users.noreply.github.com>
2026-03-16 15:49:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {menu.style.display = 'flex';}
+}
